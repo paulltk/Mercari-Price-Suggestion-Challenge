cbab5be0e57b8f921a4802a05a349db5c48cf219
paulltk <paultenkaate@outlook.com>
2018-01-15 13:59:39 +0100

    new file for all pre-proccesing funcs

--- Mercari script.py
+++ Mercari script.py
@@ -13,7 +13,7 @@ from sklearn.neural_network import MLPClassifier
 from mercarifunc import *
 
 path = "C:\\Users\pault\OneDrive\Documenten\GitHub\input" # pauls path
-path = "/home/afalbrecht/Documents/Leren en Beslissen/" #add your path here, dit is een test
+#path = "/home/afalbrecht/Documents/Leren en Beslissen/" #add your path here, dit is een test
 os.chdir(path)
 
 train = pd.read_csv('train.tsv', delimiter='\t', encoding='utf-8')
@@ -23,25 +23,14 @@ test = pd.read_csv('test.tsv', delimiter='\t', encoding='utf-8')
 print("test size:", test.shape)
 
 data = train.loc[:11000]
-#val = train.loc[0:1000]
 edit_data(data)
-#edit_data(val)
 item_des, categories, brand_names = lists_for_vector(data)
 item_dict, cat_dict, brand_dict = list_to_dict(item_des), list_to_dict(categories), list_to_dict(brand_names)
 
 matrix = instances_into_vectors(data, item_dict, cat_dict, brand_dict)
+
 val_mat = matrix[10000:11000]
 matrix = matrix[:10000]
-
-print(matrix.shape)
-print(val_mat.shape)
-
-def get_price_list(data):
-    lst = []
-    for index, row in data.iterrows():
-        lst.append(int(row["price"]))
-    return lst
-
 
 prices = get_price_list(data[:10000])
 val_prices = get_price_list(data[10000:11000])
@@ -57,18 +46,15 @@ clf = MLPClassifier(activation='relu', alpha=1e-04, batch_size='auto',
 start = time.time()
 clf.fit(matrix, prices)
 print('Model fitted', time.time() - start)
-predicted_prices = clf.predict(val_mat)
+predicted_prices = clf.predict(matrix)
 print('Prices predicted', time.time() - start)
+
 a = 0
-for price, pre_price in zip(val_prices, predicted_prices):
-    if a <100:
-        print(price, pre_price)
+i = 0
+for price, pre_price in zip(prices, predicted_prices):
+    if a <300:
+        print(i, price, pre_price)
         a += 1
+        i += 1
 
-def calc_score(prices, predicted_prices):
-    summ = 0
-    for price, pre_price in zip(prices, predicted_prices):
-        summ += (math.log(int(pre_price)+1) - math.log(int(price)+1))**2
-    return math.sqrt(summ / len(prices))
-
-print(calc_score(val_prices, predicted_prices))
+print("the score is:", calc_score(prices, predicted_prices))
---
+++ pre-proccesing.py
@@ -0,0 +1,44 @@
+def edit_data(data):
+    split_categories(data)
+    fill_missing_items(data)
+    change_item_description(data)
+
+# every item (except a few NaN's) have three categories. Here we make a list with those three categories.
+def split_categories(data):
+    data["cat_list"] = ""
+    for index, row in data.iterrows():
+        if row["category_name"] == row["category_name"]:
+            catlist = [row["category_name"].split("/")[0],
+                       row["category_name"].split("/")[1],
+                       row["category_name"].split("/")[2]]
+            # print(index, catlist)
+            data.set_value(index, "cat_list", catlist)
+        # print(row)
+    return data
+
+#replaces NaN, which is not a string, with "missing"
+def fill_missing_items(data):
+    data["brand_name"].fillna("missing", inplace=True)
+    data["item_description"].fillna("missing", inplace=True)
+
+# replaces the item description with
+def change_item_description(data):
+    wnlm = WordNetLemmatizer()
+    stopwords = set("""rm i me my myself we our ours ourselves you your yours yourself yourselves he him 
+            his himself she her hers herself it its itself they them their theirs themselves what which who whom 
+            this that these those am is are was were be been being have has had having do does did doing a an the 
+            and but if or because as until while of at by for with about against between into through during before 
+            after above below to from up down in out on off over under again further then once here there when where 
+            why how all any both each few more most other some such no nor not only own same so than too very s t can 
+            will just don should now""".split(" "))
+    for index, row in data.iterrows():
+        if isinstance(row["item_description"], list):
+            continue
+        des = row["item_description"]
+        if not isinstance(des, str):
+            print(des)
+        des = des.lower()
+        regex = re.compile('[' + re.escape(string.punctuation) + '\\r\\t\\n ]')
+        txt = regex.sub(" ", des)
+        words = [wnlm.lemmatize(w) for w in txt.split(" ") if w not in stopwords]
+        data.set_value(index, "item_description", words)
