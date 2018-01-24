897b8eab186b2552d79ed776a0a765adde9ddd3e
falksinke <falksinke@gmail.com>
2018-01-24 13:26:18 +0100

    Added Falk's path

--- New Mercari script.py
+++ New Mercari script.py
@@ -4,6 +4,7 @@ from funcs import *
 
 path = "C:\\Users\pault\OneDrive\Documenten\GitHub\input"
 #path = "/home/afalbrecht/Documents/Leren en Beslissen/"
+#path = "/Users/falksinke/LocalDocs/mercari_project"
 #path = "S:\OneDrive\Documenten\GitHub\input"
 
 os.chdir(path)
@@ -21,3 +22,50 @@ print(time.time() - start)
 sparse_matrix = make_sparse_matrix(data, description_dict, categories_dict, brand_dict)
 # print(description_dict)
 print(len(description_dict), len(category_dict), len(brand_dict))
+
+def get_rows(mat, price, rng):
+    return mat[rng].todense(), price[rng]
+
+def iter_minibatches(mat, price, chunksize):
+    # Provide chunks one by one
+    chunkstartmarker = 0
+    while chunkstartmarker < mat.shape[0]:
+        if (mat.shape[0] - chunkstartmarker) < chunksize:
+            chunksize = mat.shape[0] - chunkstartmarker
+        chunkrows = range(chunkstartmarker,chunkstartmarker+chunksize)
+        X_chunk, y_chunk = get_rows(mat, price, chunkrows)
+        yield X_chunk, y_chunk
+        chunkstartmarker += chunksize
+
+
+print("made matrix", time.time() - start)
+
+
+neuralnet = MLPRegressor(activation='relu', alpha=1e-04, batch_size=50,
+                        beta_1=0.9, beta_2=0.999, early_stopping=False,
+                        epsilon=1e-08, hidden_layer_sizes=(40,30,15), learning_rate='constant',
+                        learning_rate_init=0.001, max_iter=500, momentum=0.9,
+                        nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+                        solver='adam', tol=0.0001, validation_fraction=0.1, verbose=False,
+                        warm_start=True)
+
+batches = iter_minibatches(sparse_matrix, prices, chunksize=1000)
+
+count = 0
+for X_chunk, y_chunk in batches:
+   print(count)
+   count += 1
+   if len(X_chunk) != 0:
+        neuralnet._partial_fit(X_chunk, y_chunk)
+
+valmat = sparse_matrix[999999:].todense()
+valprices = get_price_list(train)
+print(valmat.shape)
+print(valprices.shape)
+
+predicted_prices = neuralnet.predict(valmat)
+print('Prices predicted', time.time() - start)
+
+print(valprices.shape)
+print(predicted_prices.shape)
+print("The score is:", calc_score(valprices, predicted_prices))
